b81bad1b3b859f0b9d441c029984efffafc75a2e
Harsh Nandwani <nandwaniharsh@gmail.com>
2022-03-13 12:13:31 +0530

    Add BankAccount compose screen and default_bank.png

---
+++ app/src/main/java/com/harshnandwani/digitaltijori/presentation/bank_account/add_edit/AddEditBankAccountScreen.kt
@@ -0,0 +1,12 @@
+package com.harshnandwani.digitaltijori.presentation.bank_account.add_edit
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import com.harshnandwani.digitaltijori.presentation.common_components.TopAppBarWithBackButton
+
+@Composable
+fun AddEditBankAccountScreen() {
+    Scaffold(topBar = { TopAppBarWithBackButton(title = "Provide account details") }) {
+
+    }
+}
