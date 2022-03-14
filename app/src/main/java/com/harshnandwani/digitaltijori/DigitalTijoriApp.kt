b4a078ca267e144ab0b674fe510dffffde85c537
Harsh Nandwani <nandwaniharsh@gmail.com>
2022-03-14 18:42:12 +0530

    New way to add companies to db from companies.json and also adds icon and logo resource ids along with companies

--- app/src/main/java/com/harshnandwani/digitaltijori/DigitalTijoriApp.kt
+++ app/src/main/java/com/harshnandwani/digitaltijori/DigitalTijoriApp.kt
@@ -1,7 +1,25 @@
 package com.harshnandwani.digitaltijori
 
 import android.app.Application
+import com.harshnandwani.digitaltijori.presentation.util.UpdateCompaniesOnAppStart
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class DigitalTijoriApp: Application()
+class DigitalTijoriApp : Application() {
+
+    @Inject
+    lateinit var updateCompanies: UpdateCompaniesOnAppStart
+
+    override fun onCreate() {
+        super.onCreate()
+
+        /*
+        * Called everytime app starts as icons and logos are drawable resources
+        * and it cannot be hardcoded as they might change
+        * */
+        updateCompanies.execute()
+
+    }
+
+}
