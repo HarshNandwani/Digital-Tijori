b6c15a81440ada9ef9f44d15f4a177039f6e6ac3
Harsh Nandwani <nandwaniharsh@gmail.com>
2022-03-15 16:09:40 +0530

    GetAllBankAccountsUseCase.kt added

---
+++ app/src/main/java/com/harshnandwani/digitaltijori/domain/use_case/bank_account/GetAllBankAccountsUseCase.kt
@@ -0,0 +1,14 @@
+package com.harshnandwani.digitaltijori.domain.use_case.bank_account
+
+import com.harshnandwani.digitaltijori.domain.model.BankAccount
+import com.harshnandwani.digitaltijori.domain.repository.BankAccountRepository
+import kotlinx.coroutines.flow.Flow
+
+class GetAllBankAccountsUseCase(
+    private val repository: BankAccountRepository
+) {
+    operator fun invoke(): Flow<List<BankAccount>> {
+        return repository.getAll()
+    }
+
+}
