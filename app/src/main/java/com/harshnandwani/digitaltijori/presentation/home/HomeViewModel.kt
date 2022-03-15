3718ef641f7ef1c3e8c2e686e20378901ece0272
Harsh Nandwani <nandwaniharsh@gmail.com>
2022-03-15 16:23:36 +0530

    Get all bank accounts when HomeViewModel initializes, so we can show it in BankAccountListScreen

--- app/src/main/java/com/harshnandwani/digitaltijori/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/harshnandwani/digitaltijori/presentation/home/HomeViewModel.kt
@@ -1,12 +1,17 @@
 package com.harshnandwani.digitaltijori.presentation.home
 
+import android.util.Log
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.harshnandwani.digitaltijori.domain.use_case.bank_account.GetAllBankAccountsUseCase
 import com.harshnandwani.digitaltijori.presentation.home.util.HomeScreenEvent
 import com.harshnandwani.digitaltijori.presentation.home.util.HomeScreenState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,6 +22,12 @@ class HomeViewModel @Inject constructor(
     private val _state = mutableStateOf(HomeScreenState())
     val state: State<HomeScreenState> = _state
 
+    private var getAllAccountsJob: Job? = null
+
+    init {
+        getAllBankAccounts()
+    }
+
     fun onEvent(event: HomeScreenEvent) {
         when (event) {
             is HomeScreenEvent.OnPageChanged -> {
@@ -25,6 +36,17 @@ class HomeViewModel @Inject constructor(
                 )
             }
         }
+    }
+
+    private fun getAllBankAccounts(){
+        getAllAccountsJob?.cancel()
+        getAllAccountsJob = getAllBankAccountsUseCase()
+            .onEach { accounts ->
+                _state.value = state.value.copy(
+                    bankAccounts = accounts
+                )
+            }
+            .launchIn(viewModelScope)
     }
 
 }
