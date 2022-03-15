e8c7ea3e30ba39e1635af66121019912162d34d7
Harsh Nandwani <nandwaniharsh@gmail.com>
2022-03-16 00:15:54 +0530

    Logic for edit bank account

--- app/src/main/java/com/harshnandwani/digitaltijori/presentation/bank_account/add_edit/AddEditBankAccountActivity.kt
+++ app/src/main/java/com/harshnandwani/digitaltijori/presentation/bank_account/add_edit/AddEditBankAccountActivity.kt
@@ -7,7 +7,11 @@ import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.harshnandwani.digitaltijori.domain.model.BankAccount
+import com.harshnandwani.digitaltijori.domain.model.Company
+import com.harshnandwani.digitaltijori.presentation.bank_account.add_edit.util.BankAccountEvent
 import com.harshnandwani.digitaltijori.presentation.ui.theme.DigitalTijoriTheme
+import com.harshnandwani.digitaltijori.presentation.util.Parameters
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,9 +20,21 @@ class AddEditBankAccountActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            val viewModel: AddEditBankAccountViewModel = hiltViewModel()
+
+            val mode = intent.getStringExtra(Parameters.KEY_MODE)
+            if (mode == Parameters.VAL_MODE_EDIT) {
+                val bank = intent.getSerializableExtra(Parameters.KEY_BANK) as Company
+                val account =
+                    intent.getSerializableExtra(Parameters.KEY_BANK_ACCOUNT) as BankAccount
+                viewModel.onEvent(BankAccountEvent.ChangeToEditMode(bank, account))
+
+            }
+
             DigitalTijoriTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    AddEditBankAccountScreen(hiltViewModel())
+                    AddEditBankAccountScreen(viewModel)
                 }
             }
         }
