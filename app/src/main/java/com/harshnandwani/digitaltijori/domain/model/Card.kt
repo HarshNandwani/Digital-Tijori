7cbad4a387fbdf815b5d3145b738fe53ac02ac34
Harsh Nandwani <nandwaniharsh@gmail.com>
2022-03-16 22:27:53 +0530

    Make Card model Serializable, add emptyCard() and code refactor

--- app/src/main/java/com/harshnandwani/digitaltijori/domain/model/Card.kt
+++ app/src/main/java/com/harshnandwani/digitaltijori/domain/model/Card.kt
@@ -5,6 +5,7 @@ import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import com.harshnandwani.digitaltijori.domain.util.CardNetwork
 import com.harshnandwani.digitaltijori.domain.util.CardType
+import java.io.Serializable
 
 @Entity(
     foreignKeys = [
@@ -24,16 +25,39 @@ import com.harshnandwani.digitaltijori.domain.util.CardType
 )
 data class Card(
     @PrimaryKey(autoGenerate = true)
-    val id : Int,
-    val isLinkedToBank : Boolean,
-    val bankAccountId : Int?,
+    val id: Int,
+    val isLinkedToBank: Boolean,
+    val bankAccountId: Int?,
     val companyId: Int?,
-    val cardNumber : String,
-    val expiryMonth : Byte,
+    val cardNumber: String,
+    val expiryMonth: Byte,
     val expiryYear: Byte,
-    val cvv : Short,
-    val nameOnCard : String,
+    val cvv: Short,
+    val nameOnCard: String,
     val cardNetwork: CardNetwork,
-    val cardAlias : String?,
+    val cardAlias: String?,
     val cardType: CardType,
-)
+) : Serializable {
+
+    companion object {
+
+        fun emptyCard(): Card {
+            return Card(
+                -1,
+                false,
+                null,
+                null,
+                "",
+                14,
+                1,
+                1,
+                "",
+                CardNetwork.Unknown,
+                null,
+                CardType.None
+            )
+        }
+
+    }
+
+}
