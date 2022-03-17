55ec1df29f30216e48220eee3188c1a5345f743f
Harsh Nandwani <nandwaniharsh@gmail.com>
2022-03-17 12:22:02 +0530

    CardScreen fields added

--- app/src/main/java/com/harshnandwani/digitaltijori/presentation/card/add_edit/AddEditCardActivity.kt
+++ app/src/main/java/com/harshnandwani/digitaltijori/presentation/card/add_edit/AddEditCardActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.harshnandwani.digitaltijori.presentation.ui.theme.DigitalTijoriTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AddEditCardActivity : ComponentActivity() {
+    @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
--- app/src/main/java/com/harshnandwani/digitaltijori/presentation/card/add_edit/AddEditCardScreen.kt
+++ app/src/main/java/com/harshnandwani/digitaltijori/presentation/card/add_edit/AddEditCardScreen.kt
@@ -1,8 +1,78 @@
 package com.harshnandwani.digitaltijori.presentation.card.add_edit
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.harshnandwani.digitaltijori.presentation.card.CardLayout
+import com.harshnandwani.digitaltijori.presentation.card.add_edit.util.CardEvent
+import com.harshnandwani.digitaltijori.presentation.common_components.InputTextField
 
+@ExperimentalComposeUiApi
 @Composable
 fun AddEditCardScreen(viewModel: AddEditCardViewModel) {
+
+    val state = viewModel.state.value
+
+    Column(
+        modifier = Modifier.padding(24.dp)
+    ) {
+
+        CardLayout(
+            variant = "",
+            company = state.selectedIssuer,
+            nameText = state.nameOnCard,
+            cardNumber = state.cardNumber,
+            expiryNumber = state.expiryMonth + state.expiryYear,
+            cvvNumber = state.cvv,
+            cardNetwork = state.cardNetwork
+        )
+
+        InputTextField(
+            label = "Card Number",
+            value = state.cardNumber,
+            onValueChange = { viewModel.onEvent(CardEvent.EnteredCardNumber(it)) },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            )
+        )
+
+        InputTextField(
+            label = "Card Expiry",
+            value = state.expiryMonth + state.expiryYear,
+            onValueChange = { viewModel.onEvent(CardEvent.EnteredCardExpiry(it)) },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            )
+        )
+
+        InputTextField(
+            label = "CVV",
+            value = state.cvv,
+            onValueChange = { viewModel.onEvent(CardEvent.EnteredCvv(it)) },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            )
+        )
+
+        InputTextField(
+            label = "Holder Name",
+            value = state.nameOnCard,
+            onValueChange = { viewModel.onEvent(CardEvent.EnteredNameOnCard(it)) },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Done
+            )
+        )
+
+    }
 
 }
