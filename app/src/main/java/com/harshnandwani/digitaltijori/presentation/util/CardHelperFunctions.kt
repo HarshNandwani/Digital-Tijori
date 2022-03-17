75c7367a419d7ef28441373a3cd3047488006670
Harsh Nandwani <nandwaniharsh@gmail.com>
2022-03-17 12:18:27 +0530

    CardHelperFunction

---
+++ app/src/main/java/com/harshnandwani/digitaltijori/presentation/util/CardHelperFunctions.kt
@@ -0,0 +1,21 @@
+package com.harshnandwani.digitaltijori.presentation.util
+
+import com.harshnandwani.digitaltijori.R
+import com.harshnandwani.digitaltijori.domain.util.CardNetwork
+
+class CardHelperFunctions {
+
+    companion object {
+        fun getDrawableIdForCardNetwork(cardNetwork: CardNetwork): Int {
+            return when (cardNetwork) {
+                CardNetwork.Visa -> R.drawable.visa_logo
+                CardNetwork.MasterCard -> R.drawable.mastercard_logo
+                CardNetwork.Rupay -> R.drawable.rupay_logo
+                CardNetwork.AmEx -> R.drawable.american_express_logo
+                CardNetwork.DinersClub -> R.drawable.diners_club_logo
+                CardNetwork.Unknown -> R.drawable.transparent
+            }
+        }
+    }
+
+}
