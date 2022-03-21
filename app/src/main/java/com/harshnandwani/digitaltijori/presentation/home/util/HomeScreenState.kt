d88aeea6d2a61c1e5ff29a6bb7da8fefb364a2fd
Harsh Nandwani <nandwaniharsh@gmail.com>
2022-03-21 21:27:49 +0530

    Rearranged filteredBankAccounts

--- app/src/main/java/com/harshnandwani/digitaltijori/presentation/home/util/HomeScreenState.kt
+++ app/src/main/java/com/harshnandwani/digitaltijori/presentation/home/util/HomeScreenState.kt
@@ -7,9 +7,9 @@ import com.harshnandwani.digitaltijori.domain.model.Credential
 
 data class HomeScreenState(
     val bankAccounts: Map<Company, BankAccount> = emptyMap(),
+    val filteredBankAccounts: Map<Company, BankAccount> = emptyMap(),
     val cards: List<Card> = emptyList(),
     val credentials: Map<Company, Credential> = emptyMap(),
     val currentPage: String = HomeScreens.BankAccountsList.route,
-    val filteredBankAccounts: Map<Company, BankAccount> = emptyMap(),
     val searchText: String = ""
 )
