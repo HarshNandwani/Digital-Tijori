d80a2458cac578bc0b3e026a5f8d7502e860ee5e
Harsh Nandwani <nandwaniharsh@gmail.com>
2022-03-21 21:02:10 +0530

    View Credential functionality

--- app/src/main/java/com/harshnandwani/digitaltijori/presentation/credential/detailed_view/DetailedCredentialActivity.kt
+++ app/src/main/java/com/harshnandwani/digitaltijori/presentation/credential/detailed_view/DetailedCredentialActivity.kt
@@ -6,7 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.harshnandwani.digitaltijori.domain.model.Company
+import com.harshnandwani.digitaltijori.domain.model.Credential
+import com.harshnandwani.digitaltijori.presentation.credential.detailed_view.util.DetailedCredentialEvent
 import com.harshnandwani.digitaltijori.presentation.ui.theme.DigitalTijoriTheme
+import com.harshnandwani.digitaltijori.presentation.util.Parameters
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,6 +20,10 @@ class DetailedCredentialActivity : ComponentActivity() {
         setContent {
 
             val viewModel: DetailedCredentialViewModel = hiltViewModel()
+            val entity = intent.getSerializableExtra(Parameters.KEY_ENTITY) as Company
+            val credential = intent.getSerializableExtra(Parameters.KEY_Credential) as Credential
+            viewModel.onEvent(DetailedCredentialEvent.LoadEntity(entity))
+            viewModel.onEvent(DetailedCredentialEvent.LoadCredential(credential))
 
             DigitalTijoriTheme {
                 Surface(color = MaterialTheme.colors.background) {
