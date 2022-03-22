5c45a4a0bfd2f20752d842bea1536763e6d8b045
Harsh Nandwani <nandwaniharsh@gmail.com>
2022-03-22 23:15:24 +0530

    Select issuer functionality added

--- app/src/main/java/com/harshnandwani/digitaltijori/presentation/card/CardLayout.kt
+++ app/src/main/java/com/harshnandwani/digitaltijori/presentation/card/CardLayout.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -34,7 +35,8 @@ fun CardLayout(
     cardNumber: String,
     expiryNumber: String,
     cvvNumber: String,
-    cardNetwork: CardNetwork
+    cardNetwork: CardNetwork,
+    onIssuerLogoClick: () -> Unit = {}
 ) {
 
     var backVisible by remember { mutableStateOf(false) }
@@ -86,7 +88,10 @@ fun CardLayout(
                         .constrainAs(issuerLogo) {
                             top.linkTo(parent.top)
                             end.linkTo(parent.end)
-                        },
+                        }
+                        .clickable {
+                            onIssuerLogoClick()
+                        }
                 )
 
                 Column(
--- app/src/main/java/com/harshnandwani/digitaltijori/presentation/card/add_edit/AddEditCardScreen.kt
+++ app/src/main/java/com/harshnandwani/digitaltijori/presentation/card/add_edit/AddEditCardScreen.kt
@@ -2,77 +2,113 @@ package com.harshnandwani.digitaltijori.presentation.card.add_edit
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.harshnandwani.digitaltijori.presentation.card.CardLayout
 import com.harshnandwani.digitaltijori.presentation.card.add_edit.util.CardEvent
 import com.harshnandwani.digitaltijori.presentation.common_components.InputTextField
+import com.harshnandwani.digitaltijori.presentation.company.CompaniesList
+import com.harshnandwani.digitaltijori.presentation.util.Parameters
+import kotlinx.coroutines.launch
 
+@ExperimentalMaterialApi
 @ExperimentalComposeUiApi
 @Composable
 fun AddEditCardScreen(viewModel: AddEditCardViewModel) {
 
     val state = viewModel.state.value
+    val bottomSheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
+    val coroutineScope = rememberCoroutineScope()
+    val keyboardController = LocalSoftwareKeyboardController.current
 
-    Column(
-        modifier = Modifier.padding(24.dp)
+    ModalBottomSheetLayout(
+        sheetContent = {
+            CompaniesList(
+                titleText = "Select entity",
+                companies = state.allCardIssuers,
+                onSelect = {
+                    viewModel.onEvent(CardEvent.SelectIssuer(it))
+                    coroutineScope.launch {
+                        bottomSheetState.hide()
+                    }
+                }
+            )
+        },
+        sheetState = bottomSheetState,
+        sheetShape = RoundedCornerShape(topStart = 28.dp, topEnd = 28.dp)
     ) {
+        Column(
+            modifier = Modifier.padding(24.dp)
+        ) {
 
-        CardLayout(
-            variant = "",
-            company = state.selectedIssuer,
-            nameText = state.nameOnCard,
-            cardNumber = state.cardNumber,
-            expiryNumber = state.expiryMonth + state.expiryYear,
-            cvvNumber = state.cvv,
-            cardNetwork = state.cardNetwork
-        )
-
-        InputTextField(
-            label = "Card Number",
-            value = state.cardNumber,
-            onValueChange = { viewModel.onEvent(CardEvent.EnteredCardNumber(it)) },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Next
+            CardLayout(
+                variant = "",
+                company = state.selectedIssuer,
+                nameText = state.nameOnCard,
+                cardNumber = state.cardNumber,
+                expiryNumber = state.expiryMonth + state.expiryYear,
+                cvvNumber = state.cvv,
+                cardNetwork = state.cardNetwork,
+                onIssuerLogoClick = {
+                    if(state.mode == Parameters.VAL_MODE_ADD) {
+                        keyboardController?.hide()
+                        coroutineScope.launch { bottomSheetState.show() }
+                    }
+                }
             )
-        )
 
-        InputTextField(
-            label = "Card Expiry",
-            value = state.expiryMonth + state.expiryYear,
-            onValueChange = { viewModel.onEvent(CardEvent.EnteredCardExpiry(it)) },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Next
+            InputTextField(
+                label = "Card Number",
+                value = state.cardNumber,
+                onValueChange = { viewModel.onEvent(CardEvent.EnteredCardNumber(it)) },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                )
             )
-        )
 
-        InputTextField(
-            label = "CVV",
-            value = state.cvv,
-            onValueChange = { viewModel.onEvent(CardEvent.EnteredCvv(it)) },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Next
+            InputTextField(
+                label = "Card Expiry",
+                value = state.expiryMonth + state.expiryYear,
+                onValueChange = { viewModel.onEvent(CardEvent.EnteredCardExpiry(it)) },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                )
             )
-        )
 
-        InputTextField(
-            label = "Holder Name",
-            value = state.nameOnCard,
-            onValueChange = { viewModel.onEvent(CardEvent.EnteredNameOnCard(it)) },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text,
-                imeAction = ImeAction.Done
+            InputTextField(
+                label = "CVV",
+                value = state.cvv,
+                onValueChange = { viewModel.onEvent(CardEvent.EnteredCvv(it)) },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                )
             )
-        )
 
+            InputTextField(
+                label = "Holder Name",
+                value = state.nameOnCard,
+                onValueChange = { viewModel.onEvent(CardEvent.EnteredNameOnCard(it)) },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Done
+                )
+            )
+
+        }
     }
-
 }
