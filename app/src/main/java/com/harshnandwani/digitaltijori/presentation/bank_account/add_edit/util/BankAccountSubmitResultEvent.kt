b722b81b0f052ab6802d72aa18e8af7e616c3097
Harsh Nandwani <nandwaniharsh@gmail.com>
2022-03-22 18:55:49 +0530

    Return BankAccount rowId after executing addBankAccountUseCase

--- app/src/main/java/com/harshnandwani/digitaltijori/presentation/bank_account/add_edit/util/BankAccountSubmitResultEvent.kt
+++ app/src/main/java/com/harshnandwani/digitaltijori/presentation/bank_account/add_edit/util/BankAccountSubmitResultEvent.kt
@@ -2,5 +2,5 @@ package com.harshnandwani.digitaltijori.presentation.bank_account.add_edit.util
 
 sealed class BankAccountSubmitResultEvent {
     data class ShowError(val message: String): BankAccountSubmitResultEvent()
-    object BankAccountSaved: BankAccountSubmitResultEvent()
+    data class BankAccountSaved(val accountId: Long): BankAccountSubmitResultEvent()
 }
