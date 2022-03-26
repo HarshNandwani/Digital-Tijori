3f97c1018bfdbf40174aabf0484ffcddb876e3a2
Harsh Nandwani <nandwaniharsh@gmail.com>
2022-03-26 16:25:11 +0530

    Added GetCardsLinkedToAccountUseCase

---
+++ app/src/main/java/com/harshnandwani/digitaltijori/domain/use_case/card/GetCardsLinkedToAccountUseCase.kt
@@ -0,0 +1,15 @@
+package com.harshnandwani.digitaltijori.domain.use_case.card
+
+import com.harshnandwani.digitaltijori.domain.model.Card
+import com.harshnandwani.digitaltijori.domain.repository.CardRepository
+import kotlinx.coroutines.flow.Flow
+
+class GetCardsLinkedToAccountUseCase(
+    private val repository: CardRepository
+) {
+
+    operator fun invoke(accountId: Int): Flow<List<Card>> {
+        return repository.getCardsLinkedToABank(accountId)
+    }
+
+}
