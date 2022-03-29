1ac7f5c4b677905d3c4a82599876c49fc7c5417d
Harsh Nandwani <nandwaniharsh@gmail.com>
2022-03-29 12:34:37 +0530

    Added Biometric authentication with device credential

--- app/src/main/java/com/harshnandwani/digitaltijori/presentation/MainActivity.kt
+++ app/src/main/java/com/harshnandwani/digitaltijori/presentation/MainActivity.kt
@@ -2,14 +2,18 @@ package com.harshnandwani.digitaltijori.presentation
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+import androidx.biometric.BiometricPrompt
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
 import com.harshnandwani.digitaltijori.presentation.home.HomeActivity
 import com.harshnandwani.digitaltijori.presentation.ui.theme.DigitalTijoriTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -19,11 +23,35 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-        /*
-        * This is temporary
-        * TODO: Remove this when authentication is implemented
-        * */
-        startActivity(Intent(this, HomeActivity::class.java))
-        finish()
+
+        val biometricPrompt = BiometricPrompt(this, ContextCompat.getMainExecutor(this),
+            object : BiometricPrompt.AuthenticationCallback() {
+
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    finish()
+                }
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    startActivity(Intent(this@MainActivity, HomeActivity::class.java))
+                    finish()
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    finish()
+                }
+            }
+        )
+
+        val promptInfo: BiometricPrompt.PromptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Unlock Digital Tijori")
+            .setSubtitle("Authenticate using your biometric credential")
+            .setAllowedAuthenticators(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)
+            .build()
+
+        biometricPrompt.authenticate(promptInfo)
+
     }
 }
