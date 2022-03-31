76c9e09e0b433bfaeeb854ef51b4398aa1c51160
Harsh Nandwani <nandwaniharsh@gmail.com>
2022-03-31 17:02:27 +0530

    Removed DetailedCredentialViewModel.kt state and event and finish only if context is instance of DetailedCredentialActivity

--- app/src/main/java/com/harshnandwani/digitaltijori/presentation/credential/detailed_view/DetailedCredentialActivity.kt
+++ app/src/main/java/com/harshnandwani/digitaltijori/presentation/credential/detailed_view/DetailedCredentialActivity.kt
@@ -13,41 +13,50 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.lifecycleScope
 import com.harshnandwani.digitaltijori.domain.model.Company
 import com.harshnandwani.digitaltijori.domain.model.Credential
+import com.harshnandwani.digitaltijori.domain.use_case.credential.DeleteCredentialUseCase
 import com.harshnandwani.digitaltijori.presentation.common_components.TopAppBarWithBackButton
-import com.harshnandwani.digitaltijori.presentation.credential.detailed_view.util.DetailedCredentialEvent
 import com.harshnandwani.digitaltijori.presentation.ui.theme.DigitalTijoriTheme
 import com.harshnandwani.digitaltijori.presentation.util.Parameters
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class DetailedCredentialActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var deleteCredentialUseCase: DeleteCredentialUseCase
+
     @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
-            val viewModel: DetailedCredentialViewModel = hiltViewModel()
             val entity = intent.getSerializableExtra(Parameters.KEY_ENTITY) as Company
             val credential = intent.getSerializableExtra(Parameters.KEY_Credential) as Credential
-            viewModel.onEvent(DetailedCredentialEvent.LoadEntity(entity))
-            viewModel.onEvent(DetailedCredentialEvent.LoadCredential(credential))
 
             DigitalTijoriTheme {
                 Surface(color = MaterialTheme.colors.background) {
                     Scaffold(
-                        topBar = { TopAppBarWithBackButton(title = "Account details") }
+                        topBar = { TopAppBarWithBackButton(title = "Credential details") }
                     ) {
                         Box(modifier = Modifier.fillMaxSize()) {
                             DetailedCredential(
-                                entity = viewModel.state.value.entity,
-                                credential = viewModel.state.value.credential,
+                                entity = entity,
+                                credential = credential,
                                 onDeleteAction = {
-                                    viewModel.onEvent(DetailedCredentialEvent.DeleteCredential)
-                                    Toast.makeText(this@DetailedCredentialActivity, "Credential deleted!", Toast.LENGTH_SHORT).show()
-                                    finish()
+                                    lifecycleScope.launch {
+                                        deleteCredentialUseCase(credential)
+                                        withContext(Dispatchers.Main) {
+                                            Toast.makeText(this@DetailedCredentialActivity, "Credential deleted!", Toast.LENGTH_SHORT).show()
+                                            finish()
+                                        }
+                                    }
                                 },
                                 modifier = Modifier.padding(16.dp)
                             )
